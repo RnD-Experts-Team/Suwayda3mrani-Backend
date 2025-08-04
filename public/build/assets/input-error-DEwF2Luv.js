b5317a4755ebff6a043c41a031796e561b5a609d
Aws12505 <awsaljaramani12505@gmail.com>
2025-08-04 05:46:10 -0400

    build

---
+++ public/build/assets/input-error-DEwF2Luv.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-6I9721aU.js";import{a as o}from"./button-DrU-aMAO.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
